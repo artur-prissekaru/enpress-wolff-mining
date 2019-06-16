2391e2238780c4e49617a044634c74567e0b58cf
nickjang1 <eunhyukjang1@gmail.com>
2019-06-16 15:12:21 +0700

    update contact info view map

--- resources/views/components/contact-info2.blade.php
+++ resources/views/components/contact-info2.blade.php
@@ -25,12 +25,12 @@
         </div>
       </div>
       <div class="info d-flex align-items-center">
-        <a href="https://www.google.com/maps/place/13+Jarrah+St,+Blackwater+QLD+4717/@-23.5942372,148.880728,17z/data=!3m1!4b1!4m5!3m4!1s0x6bc5daf8fc4a3fb1:0xae0d795549480323!8m2!3d-23.5942372!4d148.8829167" class="btn btn-primary">
+        <a href="https://www.google.com/maps/place/13+Jarrah+St,+Blackwater+QLD+4717/@-23.5942372,148.880728,17z/data=!3m1!4b1!4m5!3m4!1s0x6bc5daf8fc4a3fb1:0xae0d795549480323!8m2!3d-23.5942372!4d148.8829167" class="btn btn-primary" target="_blank">
           View Map
         </a>
       </div>
     </div>
-    <a href="https://www.google.com/maps/place/5+Production+Ct,+Wilsonton+QLD+4350/@-27.5462017,151.9152657,17z/data=!3m1!4b1!4m5!3m4!1s0x6b965cec026b8705:0x29d2cff4062ad03!8m2!3d-27.5462017!4d151.9174544" class="btn btn-primary">
+    <a href="https://www.google.com/maps/place/5+Production+Ct,+Wilsonton+QLD+4350/@-27.5462017,151.9152657,17z/data=!3m1!4b1!4m5!3m4!1s0x6b965cec026b8705:0x29d2cff4062ad03!8m2!3d-27.5462017!4d151.9174544" class="btn btn-primary" target="_blank">
       View Map
     </a>
   </div>
