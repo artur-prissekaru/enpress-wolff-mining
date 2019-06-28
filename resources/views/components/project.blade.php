abc2927cfb29b298be07c5de27235fb30c200e15
nickjang1 <eunhyukjang1@gmail.com>
2019-06-28 07:18:36 +0700

    update carousel width fixed

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -18,39 +18,41 @@
     }
   endif
 ?>
-<div class="card-project w-100">
-  <div class="card-image" style="background: url('{{ $data->featured_image }}')">
-    @if(isset($data->image_link) && $data->image_link !== '')
-      <a class="action-link" href="{{ $data->image_link }}" target="{{ $data->target }}">
-      </a>
-    @endif
-  </div>
-  <div class="card-body @if(isset($data->action_link) && $data->action_link !== '')has-action @endif">
-    <div class="header">
-      <div>
-        @if(isset($data->sub_title))
-          <div class="sub-title">
-            {{ $data->sub_title }}
+<a href="{{ $link }}" class="w-100 text-decoration-none" target="{{ $data->target }}">
+  <div class="card-project w-100">
+    <div class="card-image" style="background: url('{{ $data->featured_image }}')">
+      @if(isset($data->image_link) && $data->image_link !== '')
+        <button class="action-link" href="{{ $data->image_link }}" target="{{ $data->target }}">
+        </button>
+      @endif
+    </div>
+    <div class="card-body @if(isset($data->action_link) && $data->action_link !== '')has-action @endif">
+      <div class="header">
+        <div>
+          @if(isset($data->sub_title))
+            <div class="sub-title">
+              {{ $data->sub_title }}
+            </div>
+          @endif
+          <div class="title">
+            {{ $data->title }}
+          </div>
+        </div>
+        @if(isset($data->featured_logo))
+          <div class="logo">
+            <img src="{{ $data->featured_logo }}" />
           </div>
         @endif
-        <div class="title">
-          {{ $data->title }}
-        </div>
       </div>
-      @if(isset($data->featured_logo))
-        <div class="logo">
-          <img src="{{ $data->featured_logo }}" />
-        </div>
+      <div class="content">
+        @if($string)
+          {!! $string !!}
+        @endif
+      </div>
+      @if(isset($data->action_link) && $data->action_link !=='')
+        <button href="{{ $data->action_link }}" class="action-link" target="{{ $data->target }}">
+        </button>
       @endif
     </div>
-    <div class="content">
-      @if($string)
-        {!! $string !!}
-      @endif
-    </div>
-    @if(isset($data->action_link) && $data->action_link !=='')
-      <a href="{{ $data->action_link }}" class="action-link" target="{{ $data->target }}">
-      </a>
-    @endif
   </div>
-</div>
+</a>
