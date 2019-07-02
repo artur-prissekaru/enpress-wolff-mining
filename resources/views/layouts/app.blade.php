c98ab6353ae4a23e08417f5d12b47ceb32cd6688
nickjang1 <eunhyukjang1@gmail.com>
2019-07-02 11:06:29 +0700

    update bug fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,23 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('/dist/css/app.css') }}">
     <title>{{ (isset($title) ? $title : 'Wolff') }}</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142990747-1"></script>
+
+    <script>
+
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag(){dataLayer.push(arguments);}
+
+      gtag('js', new Date());
+
+    
+
+      gtag('config', 'UA-142990747-1');
+
+    </script>
     {{ wp_head() }}
 
 </head>
